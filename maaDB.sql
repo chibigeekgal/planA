e71a2c3d2637e4a8739dfbeceebb209bcd723033
Daling Zhou <dz1611@fusion27.doc.ic.ac.uk>
2013-06-02 15:19:40 +0100

    new sql file

--- maaDB.sql
+++ maaDB.sql
@@ -1,11 +1,29 @@
 DROP TABLE Person;
+DROP TABLE Question;
+DROP TABLE Answer;
 
 /*The table contains the use's login and password*/
 CREATE TABLE Person (
   Login text unique not null,
-  Pass_word text not null 
+  Pass_word text not null,
+  Points integer not null 
 );
 
----------------
-INSERT INTO Person VALUES('dz1611','123');
+/*The table contains the questions asked by each user*/
+CREATE TABLE Question (
+  Question_index integer unique not null,
+  Login text unique not null,
+  Content text not null
+);
 
+/*The table contains the answer to question*/
+CREATE TABLE Answer (
+  Question_index integer unique not null,
+  Login text unique not null,
+  Answer_content text
+);
+
+---------------------------------------------------------
+INSERT INTO Person VALUES('dz1611','123', 10);
+INSERT INTO Question VALUES(0001,'dz1611','1+2=?');
+INSERT INTO Answer VALUES(0001,'dr411','1+2=3');
