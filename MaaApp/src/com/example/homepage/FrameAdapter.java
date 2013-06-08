a9346a7d1b943af076abaab97f4b6ed86cdca659
Mike <mike@NIMBN.(none)>
2013-06-08 21:50:21 +0100

    Updated HomePage Use this Version

--- MaaApp/src/com/example/homepage/FrameAdapter.java
+++ MaaApp/src/com/example/homepage/FrameAdapter.java
@@ -1,7 +1,5 @@
 package com.example.homepage;
 
-
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,37 +8,34 @@ public class FrameAdapter extends FragmentPagerAdapter {
 
 	public FrameAdapter(FragmentManager fm) {
 		super(fm);
-		 
+
 	}
-	
-	 @Override
-     public Fragment getItem(int i) {
-         switch (i) {
-             case 0:
-                 // The first section of the app is the most interesting -- it offers
-                 // a launchpad into the other demonstrations in this example application.
-                 return new LaunchpadFragment();
 
-             default:
-                 // The other sections of the app are dummy placeholders.
-                 Fragment fragment = new subFragmentPage();
-                 Bundle args = new Bundle();
-                 args.putInt(subFragmentPage.ARG_SECTION_NUMBER, i + 1);
-                 fragment.setArguments(args);
-                 return fragment;
-         }
-     }
+	@Override
+	public Fragment getItem(int i) {
+		return new HomePageFragment();
 
-     @Override
-     public int getCount() {
-         return 3;
-     }
+	}
 
-     @Override
-     public CharSequence getPageTitle(int position) {
-         return "Section " + (position + 1);
-     }
-	
-	
+	// we have 3 different contents : Homepage,Ask,Answer
+	@Override
+	public int getCount() {
+		return 3;
+	}
+
+	// the name for each tag
+	// HomePage, Ask,Answer
+	@Override
+	public CharSequence getPageTitle(int position) {
+		switch (position) {
+		case 0:
+			return "HomePage";
+		case 1:
+			return "ASK";
+		default:
+			return "Answers";
+
+		}
+	}
 
 }
--- MaaApp/src/com/example/homepage/HomePageActivity.java
+++ MaaApp/src/com/example/homepage/HomePageActivity.java
@@ -13,62 +13,38 @@ import android.support.v4.view.ViewPager;
 public class HomePageActivity extends FragmentActivity implements
 		ActionBar.TabListener {
 
-	FrameAdapter mAppSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will display the three primary sections of the
-	 * app, one at a time.
-	 */
-	ViewPager mViewPager;
+	private FrameAdapter adapter;
+	private ViewPager viewPagerControl;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.homepage_view);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections
-		// of the app.
-		mAppSectionsPagerAdapter = new FrameAdapter(getSupportFragmentManager());
+		// adapter that return a fragment for the sections
+		adapter = new FrameAdapter(getSupportFragmentManager());
 
-		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
 
-		// Specify that the Home/Up button should not be enabled, since there is
-		// no hierarchical
-		// parent.
 		actionBar.setHomeButtonEnabled(false);
 
 		// Specify that we will be displaying tabs in the action bar.
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		// Set up the ViewPager, attaching the adapter and setting up a listener
-		// for when the
-		// user swipes between sections.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mAppSectionsPagerAdapter);
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		// ViewPager listeners setting up, attaching the adapter
+
+		viewPagerControl = (ViewPager) findViewById(R.id.pager);
+		viewPagerControl.setAdapter(adapter);
+		viewPagerControl.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 					@Override
 					public void onPageSelected(int position) {
-						// When swiping between different app sections, select
-						// the corresponding tab.
-						// We can also use ActionBar.Tab#select() to do this if
-						// we have a reference to the
-						// Tab.
 						actionBar.setSelectedNavigationItem(position);
 					}
 				});
 
 		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter.
-			// Also specify this Activity object, which implements the
-			// TabListener interface, as the
-			// listener for when this tab is selected.
+		for (int i = 0; i < adapter.getCount(); i++) {
 			actionBar.addTab(actionBar.newTab()
-					.setText(mAppSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
+					.setText(adapter.getPageTitle(i)).setTabListener(this));
 		}
 	}
 
@@ -82,13 +58,12 @@ public class HomePageActivity extends FragmentActivity implements
 			FragmentTransaction fragmentTransaction) {
 		// When the given tab is selected, switch to the corresponding page in
 		// the ViewPager.
-		mViewPager.setCurrentItem(tab.getPosition());
+		viewPagerControl.setCurrentItem(tab.getPosition());
 	}
 
 	@Override
 	public void onTabReselected(ActionBar.Tab tab,
 			FragmentTransaction fragmentTransaction) {
 	}
-	
 
 }
