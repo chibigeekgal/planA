01e70ea57750452d7fcbae0b13906bfab56e2cd3
Mike <mike@NIMBN.(none)>
2013-06-10 18:02:51 +0100

    handling AskFragment

--- MaaApp/src/com/example/homepage/HomePageFragment.java
+++ MaaApp/src/com/example/homepage/HomePageFragment.java
@@ -3,39 +3,43 @@ package com.example.homepage;
 
 import com.example.firstapp.R;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class HomePageFragment extends Fragment{
-	
+
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View homePageView = inflater.inflate(R.layout.homepage_structure, container, false);
-
+        
+        
         //select a picture 
-        homePageView.findViewById(R.id.selecting_picture)
+        homePageView.findViewById(R.id.personal_profile)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent externalActivityIntent = new Intent(Intent.ACTION_PICK);
-                        externalActivityIntent.setType("image/*");
-                        externalActivityIntent.addFlags(
-                                Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-                        startActivity(externalActivityIntent);
+                        Intent intent = new Intent(getActivity(), PersonalProfile.class);
+                        startActivity(intent);
                     }
                 });
 
         
-        
-        
+   
         
         return homePageView;
     }
 
+	
 }
