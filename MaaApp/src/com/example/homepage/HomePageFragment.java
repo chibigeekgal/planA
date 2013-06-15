00ab5bf4dca65b4b891f2af0d2c86e05c7c27912
Mike <mike@NIMBN.(none)>
2013-06-15 17:33:14 +0100

    some GUI bug fixed

--- MaaApp/src/com/example/homepage/HomePageFragment.java
+++ MaaApp/src/com/example/homepage/HomePageFragment.java
@@ -1,16 +1,12 @@
 package com.example.homepage;
 
-import com.example.firstapp.R;
-
-import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.firstapp.R;
 import com.example.firstapp.UserInfo;
