adf7f67f1ae1eb0a302a970505233ba541f52c34
Dawei Ren <dawei.ren11@imperial.ac.uk>
2013-06-16 12:50:49 +0100

    use this one

--- Server/src/Answer_Servlet.java
+++ Server/src/Answer_Servlet.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.File;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
@@ -22,73 +19,67 @@ import com.google.gson.JsonArray;
  */
 @WebServlet("/answer")
 public class Answer_Servlet extends HttpServlet {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request,
-			 HttpServletResponse response) throws ServletException, IOException {
-	response.setContentType("text/html");
-	PrintWriter out = response.getWriter();
-	out.println("<html>");
-	out.println("<head>");
-	out.println("<title> Database of Answers </title>");
-	out.println("</head>");
-	out.println("<body bgcolor=\"white\">");
-	out.println("<table>");
-	try {
-	    LinkedList<Answer_bean> answers = new AnswerMethod()
-		.getAllAnswers();
-	    for (Answer_bean answer : answers) {
-		out.println("<tr><td>" + answer.getQuestionIndex()
-			    + "</td><td>" + answer.getAnswerIndex() + "</td><td>"
-			    + answer.getContributor() + "</td><td>"
-			    + answer.getContent() + "</td></tr>");
-	    }
-	    out.println("</table>");
-	    out.println("</body>");
-	    out.println("</html>");
-	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Database of Answers </title>");
+		out.println("</head>");
+		out.println("<body bgcolor=\"white\">");
+		out.println("<table>");
+		try {
+			LinkedList<Answer_bean> answers = new AnswerMethod()
+					.getAllAnswers();
+			for (Answer_bean answer : answers) {
+				out.println("<tr><td>" + answer.getQuestionIndex()
+						+ "</td><td>" + answer.getAnswerIndex() + "</td><td>"
+						+ answer.getContributor() + "</td><td>"
+						+ answer.getContent() + "</td></tr>");
+			}
+			out.println("</table>");
+			out.println("</body>");
+			out.println("</html>");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			AnswerMethod answermethod = new AnswerMethod();
+			String query = request.getParameter("request");
+			String username = request.getParameter("username");
+			int qIndex = Integer.parseInt(request.getParameter("index"));
+			String content = request.getParameter("content");
+			PrintWriter out = response.getWriter();
+			if (query.equals("answer")) {
+				answermethod.addAnswer(username, qIndex, content);
+			}
+			if (query.equals("get_answers")) {
+				JsonArray answers = answermethod.getAnswers(qIndex);
+				out.println(answers);
+			}
+		} catch (SQLException e) {
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request,
-			  HttpServletResponse response) throws ServletException, IOException {
-	File f = new File("/homes/hs2711/planA/allanswers.txt");
-	FileWriter fw = new FileWriter(f.getAbsoluteFile());
-	BufferedWriter bw = new BufferedWriter(fw);
-	bw.write("got here....");
-	bw.close();
-	try {
-	    String query = request.getParameter("request");
-	    AnswerMethod answermethod = new AnswerMethod();
-	    String username = request.getParameter("username");
-	    int qIndex = Integer.parseInt(request.getParameter("index"));
-	    String content = request.getParameter("content");
-	    PrintWriter out = response.getWriter();
-	    if (query.equals("answer")) {
-		answermethod.addAnswer(username, qIndex, content);
-	    }
-	    if (query.equals("get_answers")) {
-			
-		JsonArray answers = answermethod.getAnswers(qIndex);
-		out.println(answers);
-	    }
-	} catch (SQLException e) {
-		    
+		}
 	}
-    }
-    
+
 }
